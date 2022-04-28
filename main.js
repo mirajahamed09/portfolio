b77ba8195a5d0a73e989d36a1d3e1ba646827f5e
ahamedmiraj49@gmail.com <ahamedmiraj49@gmail.com>
2022-04-29 00:01:56 +0530

    gh

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 console.log('Hello World!');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMP2DhGvNpKAAjraIcAjSodBP0O3EHxIM",
+  authDomain: "portfolio-f6aaa.firebaseapp.com",
+  projectId: "portfolio-f6aaa",
+  storageBucket: "portfolio-f6aaa.appspot.com",
+  messagingSenderId: "659731281113",
+  appId: "1:659731281113:web:67b232a3b2248d12293ac8",
+  measurementId: "G-Z6ELZXYMBK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 AOS.init();
 var options = {
   strings: [`I'm <span id='animation'>a Developer</span>`, `I'm <span id='animation'>a photographer</span>`,`I'm <span id='animation'>a graphic designerr</span>`],
